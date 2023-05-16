fcf783ba6175459432dc253013edc00de1992cb8
DELL <DELL@DESKTOP-DKD5K0K>
2023-05-16 09:59:54 +0530

    Add project files.

---
+++ DATA_ANVITATIONS_Practice/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using DATA_ANVITATIONS_Practice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DATA_ANVITATIONS_Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Msg = "Data Submitted Successfully";
+            }
+            else
+            {
+                ViewBag.Msg = "One (or) more validations failed";
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ DATA_ANVITATIONS_Practice/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace DATA_ANVITATIONS_Practice.Models
+{
+    public class Employee
+    {
+        [Required]
+        [RegularExpression("^[a-zA-Z]+$")]
+        public string EmployeeName { get; set; }
+        [Required]
+
+        public int EmployeeID { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Please enter a valid 10-digit phone number")]
+        [DataType(DataType.PhoneNumber)]
+        public double ContactNumber { get; set; }
+
+        [Required]
+        [StringLength(9, MinimumLength = 3)]
+        public string password { get; set; }
+        [Required]
+        [StringLength (9, MinimumLength = 3)]
+        [Compare("password")]
+
+        public string Conformpassword { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Only Acept Email Format")]
+        public string EmailID { get; set; }
+
+    }
+}
